2df06b62d0dacdd3024ae20d98aa587b6ef3d2da
muthyalasaikiran <113759393+muthyalasaikiran@users.noreply.github.com>
2024-11-19 13:00:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# main.py
-
 from fastapi import FastAPI
+import pyfiglet
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "welcome to www.aegletek.com"}
+    # Define the message
+    message = "welcome to genesys info.com"
+    
+    # Generate the ASCII art version of the message using pyfiglet
+    ascii_art = pyfiglet.figlet_format(message)
+    
+    # Return the ASCII art as part of the response
+    return {"message": ascii_art}
